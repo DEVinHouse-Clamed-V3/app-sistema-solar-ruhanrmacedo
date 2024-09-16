9bbdc1a7bc6c6c4a6ed0f546511d46473f8e6e01
Ruhan <ruhan_350@hotmail.com>
2024-09-16 00:58:49 -0300

    Importação de planetas, adicionado Scrollview, Renderização dos planetas e estilização

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+
+import planetas from './planetas';
 
 export default function App() {
   return (
@@ -8,12 +10,38 @@ export default function App() {
 
       <View style={styles.headerContainer}>
         <Text style={styles.headerText}>Vamos explorar</Text>
-
         <Image
           source={require('./assets/imageTitulo.png')}
           style={styles.image}
         />
       </View>
+
+      <ScrollView>
+        {planetas.map((planeta, index) => (
+          <View key={index} style={styles.planetContainer}>
+            <Text style={styles.planetName}>{planeta.nome}</Text>
+            <Image source={{ uri: planeta.img }} style={styles.planetImage} />
+            <View style={styles.planetDetails}>
+              <View style={styles.planetRow}>
+                <Text style={styles.planetDescription}>Average Orbital Speed:</Text>
+                <Text style={styles.planetInfo}>{planeta.velocidadeOrbitalMediaKmS} km/s</Text>
+              </View>
+              <View style={styles.planetRow}>
+                <Text style={styles.planetDescription}>Satellites:</Text>
+                <Text style={styles.planetInfo}>{planeta.quantidadeSatelites}</Text>
+              </View>
+              <View style={styles.planetRow}>
+                <Text style={styles.planetDescription}>Surface Area:</Text>
+                <Text style={styles.planetInfo}>{planeta.areaSuperficieKm2} km²</Text>
+              </View>
+              <View style={styles.planetRow}>
+                <Text style={styles.planetDescription}>Rotation Period:</Text>
+                <Text style={styles.planetInfo}>{planeta.periodoRotacaoDias} d</Text>
+              </View>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -30,6 +58,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+    marginBottom: 20,
   },
   headerText: {
     fontSize: 32,
@@ -40,5 +69,46 @@ const styles = StyleSheet.create({
   image: {
     width: 80,
     height: 80,
+  },
+  planetContainer: {
+    backgroundColor: '#000',
+    padding: 20,  
+    marginVertical: 10,
+    borderRadius: 25,
+    alignItems: 'center',
+    width: '95%',
+    alignSelf: 'center',
+  },
+  planetName: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 10,
+  },
+  planetImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    marginBottom: 20,
+  },
+  planetDetails: {
+    alignItems: 'flex-start',
+  },
+  planetRow: {
+    flexDirection: 'row',  
+    justifyContent: 'space-between',
+    width: '100%',  
+    marginBottom: 5,
+  },
+  planetDescription: {
+    color: '#9faab5',
+    fontSize: 16,
+    flex: 1,  
+  },
+  planetInfo: {
+    fontWeight: 'bold',
+    color: '#fff',
+    flex: 1,  
+    textAlign: 'right',  
   },
 });
