0c2510cbf1e1ee2123398d676dc18220565d2e4d
Ruhan <ruhan_350@hotmail.com>
2024-09-16 21:33:40 -0300

    Modificação do layout para slideshow com botões de navegação

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import planetas from './planetas';
 import Planet from './planet';
+import { useState } from 'react';
+
 
 export default function App() {
+  const [indicePlanetaAtual, setIndicePlanetaAtual] = useState(0);
+
+  const proximoPlaneta = () => {
+    if (indicePlanetaAtual < planetas.length - 1) {
+      setIndicePlanetaAtual(indicePlanetaAtual + 1);
+    }
+  };
+
+  const planetaAnterior = () => {
+    if (indicePlanetaAtual > 0) {
+      setIndicePlanetaAtual(indicePlanetaAtual - 1);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
@@ -16,21 +32,30 @@ export default function App() {
         />
       </View>
 
-      <ScrollView>
-        {planetas.map((planeta, index) => (
-          <Planet
-            key={index}
-            nome={planeta.nome}
-            img={planeta.img}
-            descricaoBreve={planeta.descricaoBreve}  
-            descricao={planeta.descricao}            
-            velocidadeOrbitalMediaKmS={planeta.velocidadeOrbitalMediaKmS}
-            quantidadeSatelites={planeta.quantidadeSatelites}
-            areaSuperficieKm2={planeta.areaSuperficieKm2}
-            periodoRotacaoDias={planeta.periodoRotacaoDias}
-          />
-        ))}
-      </ScrollView>
+      <Planet
+        nome={planetas[indicePlanetaAtual].nome}
+        img={planetas[indicePlanetaAtual].img}
+        descricaoBreve={planetas[indicePlanetaAtual].descricaoBreve}
+        descricao={planetas[indicePlanetaAtual].descricao}
+        velocidadeOrbitalMediaKmS={planetas[indicePlanetaAtual].velocidadeOrbitalMediaKmS}
+        quantidadeSatelites={planetas[indicePlanetaAtual].quantidadeSatelites}
+        areaSuperficieKm2={planetas[indicePlanetaAtual].areaSuperficieKm2}
+        periodoRotacaoDias={planetas[indicePlanetaAtual].periodoRotacaoDias}
+      />
+
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Anterior"
+          onPress={planetaAnterior}
+          disabled={indicePlanetaAtual === 0}
+        />
+        <Button
+          title="Próximo"
+          onPress={proximoPlaneta}
+          disabled={indicePlanetaAtual === planetas.length - 1}
+        />
+      </View>
+
     </View>
   );
 }
@@ -58,5 +83,11 @@ const styles = StyleSheet.create({
   image: {
     width: 80,
     height: 80,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '60%',
+    marginTop: 20,
   },
 });
